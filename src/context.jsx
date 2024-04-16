98e1e8d57af2eb9c98ed1368de3bed467bb295da
zen <zenon.deviagge@gmail.com>
2024-04-16 18:03:09 -0300

    Test and delete global `greeting` variable. Setup useReducer in context.jsx. Define state and dispatch variables. Create and export reducer function to reducer.js.

--- src/context.jsx
+++ src/context.jsx
@@ -1,11 +1,22 @@
 import { useContext, useReducer, useEffect, createContext } from 'react'
+import reducer from './reducer'
 
 const AppContext = createContext()
 
+
+
+const initialState = {
+  loading: false,
+  cart:[]
+}
+
 export const AppProvider = ({ children }) => {
-  const greeting = 'hello'
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
+
+
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
   )
 }
 
