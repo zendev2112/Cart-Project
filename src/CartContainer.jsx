e4688942a3c9eeb21f694c7f620afb6ece593695
zen <zenon.deviagge@gmail.com>
2024-04-16 22:21:44 -0300

    Refactor data from Array to Map

---
+++ src/CartContainer.jsx
@@ -0,0 +1,53 @@
+import CartItem from './CartItem'
+import { useGlobalContext } from './context'
+import cartItems from './data'
+
+const CartContainer = () => {
+  const { cart } = useGlobalContext()
+
+  const cartArray = Array.from(cart.entries())
+
+  if (cartArray.length === 0) {
+    return (
+      <section className="cart">
+        {/* cart header */}
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    )
+  }
+  return (
+    <section className="cart">
+      {/* cart header */}
+      <header>
+        <h2>your bag</h2>
+      </header>
+      {/* cart items */}
+      <div>
+        {cartArray.map((cartItem) => {
+          const [id,item] = cartItem;
+          return <CartItem key={id} {...item} />
+        })}
+      </div>
+      {/* cart footer */}
+      <footer>
+        <hr />
+        <div>
+          <h5 className="cart-total">
+            total <span>$10</span>
+          </h5>
+        </div>
+        <button
+          className="btn btn-hipster"
+          onClick={() => console.log('clear cart')}
+        >
+          clear cart
+        </button>
+      </footer>
+    </section>
+  )
+}
+
+export default CartContainer
--- src/context.jsx
+++ src/context.jsx
@@ -1,26 +1,27 @@
 import { useContext, useReducer, useEffect, createContext } from 'react'
 import reducer from './reducer'
-import { CLEAR_CART, REMOVE, INCREASE, DECREASE, LOADING, DISPLAY_ITEMS } from './actions';
+import cartItems from './data'
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from './actions'
 
 const AppContext = createContext()
 
-
-
 const initialState = {
   loading: false,
-  cart:[]
-};
-
-
-
+  cart: new Map(cartItems.map((item)=>[item.id,item])),
+}
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
-  
-
 
   return (
-    <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   )
 }
 
