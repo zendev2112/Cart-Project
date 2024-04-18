2d286091405d882ccea262947d014aeeb880c38a
zen <zenon.deviagge@gmail.com>
2024-04-18 00:41:07 -0300

    Dispatch DISPLAY_ITEMS action on global context.js, with cart state as payload. In reducer change loading from true to false, map over the fetched data and display it.

--- src/context.jsx
+++ src/context.jsx
@@ -16,40 +16,53 @@ const url = 'https://www.course-api.com/react-useReducer-cart-project'
 const AppContext = createContext()
 
 const initialState = {
-  loading: true,
-  cart: new Map(cartItems.map((item) => [item.id, item])),
+  loading: false,
+  cart: new Map(),
 }
 
 export const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-  const {totalAmount, totalCost} = getTotals(state.cart)
+  const { totalAmount, totalCost } = getTotals(state.cart)
 
   const clearCart = () => {
     dispatch({ type: CLEAR_CART })
   }
 
   const remove = (id) => {
-    dispatch({type: REMOVE, payload:{id}})
+    dispatch({ type: REMOVE, payload: { id } })
   }
 
   const increase = (id) => {
     dispatch({ type: INCREASE, payload: { id } })
   }
 
-    const decrease = (id) => {
+  const decrease = (id) => {
     dispatch({ type: DECREASE, payload: { id } })
   }
 
-  const fetchData = async() => {
-    const response = await fetch(url);
-    const cart = await response.json();
-    console.log(cart); 
+  const fetchData = async () => {
+    dispatch({ type: LOADING })
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({type: DISPLAY_ITEMS, payload:{cart}})
   }
-  useEffect(() => {},[])
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
-    <AppContext.Provider value={{ ...state, clearCart, remove, increase, decrease, totalAmount, totalCost }}>
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        totalAmount,
+        totalCost,
+      }}
+    >
       {children}
     </AppContext.Provider>
   )
